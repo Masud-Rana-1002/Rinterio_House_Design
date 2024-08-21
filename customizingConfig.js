d18aca6c07df3a501b2ed68db10eca6cd7365c1a
Masud-Rana-1002 <masudrana19981002@gmail.com>
2024-08-21 18:33:14 +0300

    nav bar and banner section add

---
+++ customizingConfig.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          customGreen: '#ABEF5F',
+          btn_sc : '#ABEF5F80',
+          btn_sc_arrow : '#ABEF5F'
+        }
+      }
+    }
+  }
