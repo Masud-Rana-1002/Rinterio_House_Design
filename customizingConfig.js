f611edab47c53d79e7397e9d7b34ea16b97c3210
Masud-Rana-1002 <masudrana19981002@gmail.com>
2024-08-22 22:11:09 +0300

    Meet Our Real Estate Team section add

--- customizingConfig.js
+++ customizingConfig.js
@@ -2,7 +2,6 @@ tailwind.config = {
     theme: {
       extend: {
         colors: {
-          clifford: '#da373d',
           customGreen: '#ABEF5F',
           btn_sc : '#ABEF5F80',
           btn_sc_arrow : '#ABEF5F'
