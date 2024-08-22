f68a7ada6f84e951cf1adf78cf8b467361763148
Masud-Rana-1002 <masudrana19981002@gmail.com>
2024-08-22 18:10:52 +0300

    form section add

--- customizingConfig.js
+++ customizingConfig.js
@@ -6,6 +6,12 @@ tailwind.config = {
           customGreen: '#ABEF5F',
           btn_sc : '#ABEF5F80',
           btn_sc_arrow : '#ABEF5F'
+        },
+        fontFamily:{
+          manrope :"Manrope"
+        },
+        backgroundImage :{
+          project_bg : "url(./assets/bg.jpg)"
         }
       }
     }
